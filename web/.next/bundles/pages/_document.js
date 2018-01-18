baf2817021a68fccdfce980186c41f8b85deedc2
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-01-18 10:52:00 +0100

    cleanup

--- web/.next/bundles/pages/_document.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/_document', function() {
-            var comp = module.exports=webpackJsonp([2],[],[226]);
-            return { page: comp.default }
-          })
-        
