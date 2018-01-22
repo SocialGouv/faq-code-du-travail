eb6681af23a6d25446c122a0efab4d129d946540
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-01-22 10:48:05 +0100

    fix dev/prod urls for github pages

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,15 @@
+// si on est hebergé sur github.io on est dans un sous-dossier...
+const baseUrl = process.env.NODE_ENV === "production" ? "/faq-code-du-travail/" : "";
+
 module.exports = {
   exportPathMap: function() {
     return {
       "/": { page: "/" }
     };
   },
-  assetPrefix: "/faq-code-du-travail/"
+  assetPrefix: baseUrl,
+  webpack: config => {
+    config.output.publicPath = `${baseUrl}${config.output.publicPath}`;
+    return config;
+  }
 };
